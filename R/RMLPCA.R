480d746462e3c53567fe6455903d98bea2e4d82c
renanestatcamp <renan@estatcamp.com.br>
2020-10-14 21:02:05 -0300

    # added doc to RMLPCA

---
+++ R/RMLPCA.R
@@ -0,0 +1,9 @@
+#' RMLPCA: A package for computating MLPCA algorithms b,c,d and e
+#'
+#' The RMLPCA package provides four algorithms that to deals with measurement
+#' errors
+#'
+#' @docType package
+#' @name RMLPCA
+NULL
+#> NULL
